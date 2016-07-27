dc3ac9fddc7aa8fa95854a500c2d28d1c5c2d075
cheese.cake <cheese.cake@kakaocorp.com>
2016-07-28 01:27:17 +0900

    first commit

---
+++ set/set.py
@@ -0,0 +1,14 @@
+#!/usr/bin/py
+import unittest
+
+class StackTestCase(unittest.TestCase):
+
+    # Introduction to Sets
+    # https://www.hackerrank.com/challenges/py-introduction-to-sets
+    # average 구하기
+    def test_introduction_to_sets(self):
+        print("fill here with awesome code")
+
+
+if __name__ == '__main__':
+    unittest.main()
